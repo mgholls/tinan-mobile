7e0f6ee2ee57810708d850c6ab6057572c1dcfb7
Martin Hollingsworth <martinh.reg@gmail.com>
2015-01-07 11:35:01 +1100

    Initial Cordova application creation

---
+++ client/InitialiseCordova.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'
+
+Push-Location C:\Working\github\tinan-mobile\client
+cordova create src net.tinan.mobile Tinan
+Set-Location .\src
+cordova platform add android
+cordova platform add browser
+cordova run android
+Set-Location C:\Working\github\tinan-mobile
+$psISE.CurrentFile.Save()
